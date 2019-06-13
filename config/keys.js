a1aaa399af0dcdf643da9593ba140f9b3274eb5d
COINMAC Inc <coinmacsms@gmail.com>
2019-06-13 12:54:03 +0100

    Added New Mongo DB

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-    MongoURI : 'mongodb://localhost:27017/coinmac?retryWrites=true'
+    // MongoURI : 'mongodb://localhost:27017/coinmac?retryWrites=true'
+    MongoURI : 'mongodb+srv://cproperties:@@coinsms22@coinmac-grgfi.mongodb.net/test?retryWrites=true&w=majority'
     // MongoURI : 'mongodb://username:password@localhost:27017/coinmac?retryWrites=true'
     // MongoURI : 'mongodb+srv://cproperties:Oyinkansola1@cproperties-didhv.mongodb.net/cproperties?retryWrites=true'
 }
